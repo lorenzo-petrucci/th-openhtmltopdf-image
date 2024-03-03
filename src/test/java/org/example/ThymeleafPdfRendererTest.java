5449cf73a5fa5614cee0d0b3c641a903e3213654
Lorenzo Petrucci <lorenzopetrucci.91@gmail.com>
2024-03-03 23:47:51 +0100

    :white_check_mark: update test

--- src/test/java/org/example/ThymeleafPdfRendererTest.java
+++ src/test/java/org/example/ThymeleafPdfRendererTest.java
@@ -8,13 +8,13 @@ import org.thymeleaf.context.Context;
 import java.io.*;
 import java.util.Map;
 
-public class PdfRendererTest {
+public class ThymeleafPdfRendererTest {
     final String DIR = "/home/lorenzo/IdeaProjects/openhtmltopdf-image/src/main/resources/";
     final String SRC = "/home/lorenzo/IdeaProjects/openhtmltopdf-image/src/main/resources/template.html";
     final String DST = "/home/lorenzo/IdeaProjects/openhtmltopdf-image/src/main/resources/out.pdf";
     //FIXME: spring test for properties?
 
-    private PdfRenderer renderer;
+    private ThymeleafPdfRenderer renderer;
     private InputStream is;
     private OutputStream os;
     private Context ctx;
@@ -24,8 +24,12 @@ public class PdfRendererTest {
         is = new FileInputStream(SRC);
         os = new FileOutputStream(DST);
         ctx = new Context();
-        ctx.setVariables(Map.of("key", (Object) "value"));
-        renderer = new PdfRenderer(DIR, is, os);
+        ctx.setVariables(Map.of("key", "value"));
+
+        renderer = new ThymeleafPdfRenderer(new TemplateEngine());
+        renderer.setSource(DIR);
+        renderer.setInputStream(is);
+        renderer.setOutputStream(os);
     }
 
     @After
@@ -36,13 +40,6 @@ public class PdfRendererTest {
 
     @Test
     public void pdfGenerationFromTemplate() throws IOException {
-        final var engine = new TemplateEngine();
-        renderer.createPDF(engine, ctx);
+        renderer.createPdf(ctx);
     }
-
-    @Test
-    public void generatePdfFromThymeleaf() throws IOException {
-        renderer.createThymeleafPDF(ctx);
-    }
-
 }
