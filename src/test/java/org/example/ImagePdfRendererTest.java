60e25fe2e11f4177b92075186d31616f0426c0b9
Lorenzo Petrucci <lorenzopetrucci.91@gmail.com>
2024-03-03 20:00:49 +0100

    :white_check_mark: costant src, out param in test

--- src/test/java/org/example/ImagePdfRendererTest.java
+++ src/test/java/org/example/ImagePdfRendererTest.java
@@ -9,25 +9,23 @@ import java.io.IOException;
 import java.util.Map;
 
 public class ImagePdfRendererTest {
+    final String SRC = "/home/lorenzo/IdeaProjects/openhtmltopdf-image/src/main/resources/template";
+    final String OUT = "/home/lorenzo/IdeaProjects/openhtmltopdf-image/src/main/resources/out";
+    //FIXME: spring test for environment vars?
+
     ImagePdfRenderer renderer;
 
     @Before
     public void setup() {
-        renderer = new ImagePdfRenderer(
-                "/home/lorenzo/IdeaProjects/openhtmltopdf-image/src/main/resources/template",
-                "/home/lorenzo/IdeaProjects/openhtmltopdf-image/src/main/resources/out"
-        );
-        //FIXME: spring test for environment vars?
+        renderer = new ImagePdfRenderer(SRC, OUT);
     }
 
     @Test
     public void pdfGenerationFromTemplateWithImageTest() throws IOException {
+        final var engine = new TemplateEngine();
         final var ctx = new Context();
         final var variables = Map.of("key", (Object) "value");
         ctx.setVariables(variables);
-
-        final var engine = new TemplateEngine();
-
         renderer.createPDF(engine, ctx);
     }
 }
