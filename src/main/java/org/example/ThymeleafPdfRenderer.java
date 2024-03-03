7ef71b1830b2eb32b2e27cbe0566f60538181850
Lorenzo Petrucci <lorenzopetrucci.91@gmail.com>
2024-03-03 23:40:25 +0100

    :recycle: template engine in constructor, getters and setters for other params

--- src/main/java/org/example/ThymeleafPdfRenderer.java
+++ src/main/java/org/example/ThymeleafPdfRenderer.java
@@ -7,30 +7,48 @@ import org.thymeleaf.context.Context;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
-public class PdfRenderer {
-    private final String src;
-    private final InputStream is;
-    private final OutputStream os;
+public class ThymeleafPdfRenderer {
+    private final TemplateEngine engine;
+    private String source;
+    private InputStream inputStream;
+    private OutputStream outputStream;
 
-    public PdfRenderer(String src, InputStream is, OutputStream os) throws IOException {
-        this.src = src;
-        this.is = is;
-        this.os = os;
+    public ThymeleafPdfRenderer(TemplateEngine engine) {
+        this.engine = engine;
     }
 
-    public void createPDF(TemplateEngine engine, Context ctx) throws IOException {
-        final String template = new String(is.readAllBytes(), StandardCharsets.UTF_8);
-        final String html = engine.process(template, ctx);
+    public void createPdf(Context ctx) throws IOException {
+        final String template = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+        final String html = this.engine.process(template, ctx);
         final var builder = new PdfRendererBuilder();
-        builder.withHtmlContent(html, src);
-        builder.toStream(os);
+        builder.withHtmlContent(html, source);
+        builder.toStream(outputStream);
         builder.run();
         // TODO: add more settings with builder
     }
 
-    public void createThymeleafPDF(Context ctx) throws IOException {
-        final var template = new TemplateEngine();
-        createPDF(template, ctx);
+    public String getSource() {
+        return source;
+    }
+
+    public InputStream getInputStream() {
+        return inputStream;
+    }
+
+    public OutputStream getOutputStream() {
+        return outputStream;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public void setInputStream(InputStream inputStream) {
+        this.inputStream = inputStream;
+    }
+
+    public void setOutputStream(OutputStream outputStream) {
+        this.outputStream = outputStream;
     }
 }
 
